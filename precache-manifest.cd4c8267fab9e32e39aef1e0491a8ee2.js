0d2d1dfdfcd488ff057b11f173e69387a1fa14f6
Junaid <junaidansari3020@gmail.com>
2022-04-26 16:19:06 +0530

    Updates

---
+++ precache-manifest.cd4c8267fab9e32e39aef1e0491a8ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b908ff7c7ab7fd06b932",
+    "url": "/TextUtils-React/static/css/main.85336bfc.chunk.css"
+  },
+  {
+    "revision": "b908ff7c7ab7fd06b932",
+    "url": "/TextUtils-React/static/js/main.b908ff7c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "9f467190b4df4fe2318e",
+    "url": "/TextUtils-React/static/js/2.9f467190.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "80298cf25a966baf3f72ae12cd73802f",
+    "url": "/TextUtils-React/index.html"
+  }
+];
